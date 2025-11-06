74381d7bbc5d4876e0df7f6ac03874ffe4bd116b
Ivan Saenko <tigersay@yandex.ru>
2025-11-06 12:38:07 +0300

    fix relative paths

--- run_scripts/run_client_proxy.sh
+++ run_scripts/run_client_proxy.sh
@@ -1,4 +1,4 @@
-python ../src/http_proxy.py --mode client \
+python src/http_proxy.py --mode client \
 --listen-port 9000 \
 --target-url https://your-try-cloudflare-url.trycloudflare.com \
 --key 32_bit_encryption_key_1234567890 \
--- run_scripts/run_server_proxy.sh
+++ run_scripts/run_server_proxy.sh
@@ -1,4 +1,4 @@
-python ../src/http_proxy.py --mode server \
+python src/http_proxy.py --mode server \
 --listen-port 9000 \
 --target-url http://localhost:8001 \
 --key 32_bit_encryption_key_1234567890 \
