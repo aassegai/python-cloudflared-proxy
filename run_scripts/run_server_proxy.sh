b964dc1479411181bf996569a25f58364176dfd0
Andrey Nepomnyashchy <chebupelka4@gmail.com>
2025-11-07 08:18:37 +0300

    fix auth

--- run_scripts/run_server_proxy.sh
+++ run_scripts/run_server_proxy.sh
@@ -2,4 +2,5 @@ python src/http_proxy.py --mode server \
 --listen-port 9000 \
 --target-url http://localhost:8001 \
 --key 32_bit_encryption_key_1234567890 \
---iv 16_bit_key_123456 
+--iv 16_bit_key_123456 \
+--logs-path "../logs_cloudflared/server.log"
